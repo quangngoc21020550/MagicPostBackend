0732e677893b800fb3fc267d4792243bc5735f91
Quang Ngoc <21020550@vnu.edu.vn>
2023-11-06 15:49:44 +0700

    upd db url

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,12 +1,13 @@
 import pymongo
-
+# from pymongo.server_api import ServerApi
 
 from app.config import MONGO_DB_NAME, MONGO_DB_URL
-
+# import dnspython
 print(MONGO_DB_NAME)
 print(MONGO_DB_URL)
 
 client = pymongo.MongoClient(MONGO_DB_URL)
+# client = pymongo.MongoClient(MONGO_DB_URL, server_api=pymongo.ServerApi('1'))
 db = client[MONGO_DB_NAME]
 
 
--- app/models/modelbase.py
+++ app/models/modelbase.py
@@ -8,13 +8,14 @@
 # }
 
 
-import pymongo
-import jsonschema
-from jsonschema import validate
-import json
+import traceback
 import uuid
-# from app.callcache import dict_cache
 
+import jsonschema
+import pymongo
+from jsonschema import validate
+from pymongo import UpdateOne
+import time
 
 
 class modelBase:
@@ -26,8 +27,13 @@ class modelBase:
         self.schema = kwargs.get("schema") if kwargs.get('schema') else None
         self.schemasearch = kwargs.get("schemasearch") if kwargs.get('schemasearch') else None
 
+
     def getModel(self):
         return self.db[self.clname]
+
+    # mongo.db.get_collection
+
+
 
     def search(self,*args,**kwargs):
         try:
@@ -38,27 +44,25 @@ class modelBase:
             content = request_json.get('content') if request_json.get('content') else None
             page = int(request_json.get('pageindex')) if 'pageindex' in request_json.keys() else None
             pagesize = int(request_json.get('pagesize')) if 'pagesize' in request_json.keys() else None
+            searchnone = kwargs.get("searchnone") if 'searchnone' in kwargs.keys() else False
 
             if not (self.schemasearch == None or self.schemasearch == "") and self.validateJson(content,self.schemasearch):
                 return (400,"JsonSchema failed validation")
 
 
             if (not page or page ==0): page=1
-            if (not pagesize or page ==0): pagesize=10
+            if (not pagesize or page ==0): pagesize=100000
 
 
 
             channelCrs = None
             if content != None and content != {}:
-                for k, v in list(content.items()):
-                    if v == None:
-                        content.pop(k)
-
+                if searchnone == False:
+                    for k, v in list(content.items()):
+                        if v == None:
+                            content.pop(k)
                 channelCrs = self.db[self.clname].find(content).limit(pagesize).skip((page-1)*pagesize)
             else:
-                # print("2")
-
-
                 channelCrs = self.db[self.clname].find().limit(pagesize).skip((page - 1) * pagesize)
 
             out = []
@@ -87,20 +91,25 @@ class modelBase:
                 return (400, "JsonSchema failed validation")
 
             # print(self.clname)
-
-            try:
-                idss = request_json['_id']
-            except:
+            if "_id" not in request_json:
                 idss = str(uuid.uuid4())
-                request_json['_id'] = idss
-
+            else:
+                idss = request_json["_id"]
             # print(idss)
 
             request_json['_id'] = idss
-            docinserted = self.db[self.clname].insert(request_json)
-            if docinserted == idss:
-                it = self.db[self.clname].find_one({'_id':docinserted})
-                # dict_cache.runApp()
+            if "createdate" not in request_json:
+                 request_json['createdate']= round(time.time() * 1000)
+            elif request_json["createdate"] == 0:
+                request_json['createdate'] = round(time.time() * 1000)
+            if "updatedate" not in request_json:
+                request_json['updatedate']= None
+            elif request_json["updatedate"] == 0:
+                request_json['updatedate'] = None
+
+            docinserted = self.db[self.clname].insert_one(request_json)
+            if docinserted.inserted_id == idss:
+                it = self.db[self.clname].find_one({'_id': idss})
 
                 return (200,it)
             else:
@@ -117,13 +126,13 @@ class modelBase:
             id = request_json['_id']
             docdeleted = self.db[self.clname].delete_one({'_id':id})
             if docdeleted.deleted_count ==1:
-                # dict_cache.runApp()
                 return (200, {"_id":id})
             else:
-                return (400, {"_id": None})
+                return (200, {"_id": None})
             # print(docdeleted.deleted_count)
 
         except Exception as e:
+            traceback.print_exc()
             return (400,str(e))
 
 
@@ -132,17 +141,42 @@ class modelBase:
 
         try:
             request_json = kwargs.get("json")
+            id = request_json['_id']
             if self.schema != None and self.validateJson(request_json, self.schema) == False:
                 return (400, "JsonSchema failed validation")
-            id = request_json['_id']
-            docinserted = self.db[self.clname].update({'_id': id}, request_json, upsert=False)
-            # dict_cache.runApp()
-            doc = self.db[self.clname].find_one({'_id': id})
-            return (200,doc)
+            request_json['updatedate'] = round(time.time() * 1000)
+
+            # print(request_json)
+
+            docinserted = self.db[self.clname].update_one({'_id': id},{"$set": request_json}, upsert=False)
+            return (200,request_json)
         except Exception as e:
             return (400,str(e))
 
+    def aggregate_search(self,*args,**kwargs):
+        try:
 
+            request_json = kwargs.get("json")
+
+            content = request_json.get('content') if request_json.get('content') else None
+            if not (self.schemasearch == None or self.schemasearch == "") and self.validateJson(content,self.schemasearch):
+                return (400,"JsonSchema failed validation")
+
+            channelCrs = self.db[self.clname].aggregate(content)
+
+
+            out = []
+            try:
+                while True:
+                    out.append(channelCrs.next())
+            except StopIteration:
+                # do nothing
+                pass
+            return (200, out)
+
+            # return (200,uid)
+        except Exception as e:
+            return (400, str(e))
 
     def findAllData(self):
         try:
@@ -169,4 +203,60 @@ class modelBase:
         except jsonschema.exceptions.ValidationError as err:
             return False
         return True
+
+    def insert_many_docs(self, *args, **kwargs):
+
+        # print(idss)
+
+        try:
+            list_request_json = list(kwargs.get("json"))
+
+            if self.schema != None and self.validateJson(list_request_json, self.schema) == False:
+                return (400, "JsonSchema failed validation")
+
+            # print(self.clname)
+            listidss= []
+            for request_json in list_request_json:
+                if "_id" not in request_json:
+                    idss = str(uuid.uuid4())
+                    listidss.append(idss)
+                else:
+                    idss = request_json["_id"]
+                    listidss.append(idss)
+
+                # print(idss)
+                request_json['createdate'] = round(time.time() * 1000)
+                request_json['updatedate'] = None
+                request_json['_id'] = idss
+            docinserted = self.db[self.clname].insert_many(list_request_json)
+            if docinserted.inserted_ids == listidss:
+                it = self.db[self.clname].find({'_id': {"$in":docinserted.inserted_ids}})
+                out = []
+                try:
+                    while True:
+                        out.append(it.next())
+                except StopIteration:
+                    # do nothing
+                    pass
+                return (200, out)
+            else:
+                return (400, docinserted)
+        except pymongo.errors.DuplicateKeyError as e:
+            self.insert_doc(request_json)
+    def update_many_docs(self,*args,**kwargs):
+
+
+        try:
+            list_request_json = list(kwargs.get("json"))
+            list_id= list(map(lambda x: x["_id"],list_request_json))
+                # id = request_json['_id']
+            if self.schema != None and self.validateJson(list_request_json, self.schema) == False:
+                    return (400, "JsonSchema failed validation")
+            updates=[]
+            for item in list_request_json:
+                updates.append(UpdateOne({'_id': item["_id"]}, {"$set":item}))
+            docinserted = self.db[self.clname].bulk_write(updates)
+            return (200,docinserted)
+        except Exception as e:
+            return (400,str(e))
 
