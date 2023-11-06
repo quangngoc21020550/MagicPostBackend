e008817916de8dcb8e44ceb7172c514982ea1046
Quang Ngoc <21020550@vnu.edu.vn>
2023-11-06 15:07:55 +0700

    first commint

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ app.add_middleware(
 
 app.include_router(product_router, prefix="/test")
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
     # print("running")
